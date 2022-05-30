a153896b3d14126fd19a1b5384e16b233077448a
Joseph Bravo <josephangelobravo@gmail.com>
2022-05-30 16:45:22 +0000

    use strict equality on hex number check

--- client/lib/rgb-to-hex.js
+++ client/lib/rgb-to-hex.js
@@ -1,6 +1,6 @@
 function colorToHex(color) {
   const hexadecimal = color.toString(16);
-  return hexadecimal.length == 1 ? '0' + hexadecimal : hexadecimal;
+  return hexadecimal.length === 1 ? '0' + hexadecimal : hexadecimal;
 }
 
 export default function convertRGBtoHex(red, green, blue) {
